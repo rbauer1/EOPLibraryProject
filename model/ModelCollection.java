8d42cc824154b47133dcf1ab595dd7ff94ad9c30
unknown <mandr5@DRK45_27F3.win.brockport.edu>
2014-04-01 12:54:06 -0400

    Comment ModelCollection Add Validate method to ModelCollection

--- model/ModelCollection.java
+++ model/ModelCollection.java
@@ -1,3 +1,12 @@
+/**
+ * COPYRIGHT 2014 Sandeep Mitra and students 
+ * The College at Brockport, State University of New York.
+ * ALL RIGHTS RESERVED
+ * 
+ * This file is the product of The College at Brockport and cannot
+ * be reproduced, copied, or used in any shape or form without
+ * he express written consent of The College at Brockport. * 
+ */
 package model;
 
 import java.util.ArrayList;
@@ -6,48 +15,51 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Properties;
 
+/**
+ * Superclass for all collections of models.
+ * Provides methods for finding/adding/sorting entities
+ */
 public abstract class ModelCollection<T extends Model> extends EntityBase {
 	
+	/** Holds all the models entities */
 	private List<T> entities;
 
+	/**
+	 * Constructs a new Model Collection
+	 */
 	protected ModelCollection() {
 		super();
 		this.entities = new ArrayList<T>();
 	}
 	
-	protected abstract Properties getSchema();
-	protected abstract String getTableName();
-	protected abstract T createEntity(Properties persistentState);
-
-	protected void setEntitiesFromQueryResult(List<?> result){
-		this.entities = new ArrayList<T>();
-		if (result != null) {
-			for(Object state : result){
-				if(state instanceof Properties){
-					T entity = this.createEntity((Properties)state);
-					entity.setPersisted(true);
-					add(entity);
-				}
-			}
-		}
-	}
-	
-	public List<T> getEntities() {
-		return this.entities;
-	}
-	
+	/**
+	 * Fills collection with all entities in table
+	 */
 	public void findAll() {
 		findByQuery("SELECT * FROM " + getTableName());
 	}
 	
+	/**
+	 * Fills collection with all entities in table that match provided whereClause
+	 * @param whereClause
+	 */
 	public void find(String whereClause) {
 		findByQuery("SELECT * FROM " + getTableName() + " " + whereClause);
 	}
 	
+	/**
+	 * Fills collection with all entities in table that match provided whereClause
+	 * @param whereClause - properties object where keys are column names
+	 */
 	public void find(Properties whereClause) {
 		setEntitiesFromQueryResult(getPersistentState(getSchema(), whereClause));
 	}
 	
+	/**
+	 * Fills collection with all entities in table where column of name key == value
+	 * @param key - column name
+	 * @param value - column value
+	 */
 	public void findByKey(String key, String value) {
 		if(key == null || key.length() == 0 || value == null || value.length() == 0){
 			throw new IllegalArgumentException("Key and value must not be empty.");
@@ -57,18 +69,115 @@ public abstract class ModelCollection<T extends Model> extends EntityBase {
 		find(whereClause);
 	}
 	
+	/**
+	 * Fills collection with results from provided query
+	 * @param query
+	 */
 	protected void findByQuery(String query){
 		setEntitiesFromQueryResult(getSelectQueryResult(query));
 	}
 	
+	/**
+	 * Adds model entity to collection. This does not modify or save to the db. 
+	 * @param entity
+	 */
+	public void add(T entity) {
+		if(entity != null){
+			this.entities.add(entity);
+		}
+	}
+
+	/**
+	 * Get entity in collection where entity attribute referenced by key == value
+	 * If more than one entry exists the first one is retrieved.
+	 * @param key
+	 * @param value
+	 */
+	public T get(String key, Object value) {
+		for (T entity : this.entities) {
+			if (entity.getState(key).equals(value)) {
+				return entity;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Removes first occurrence from collection where entity attribute referenced by key == value
+	 * @param key
+	 * @param value
+	 */
+	public T remove(String key, Object value) {
+		int index = 0;
+		for (T entity : this.entities) {
+			if (entity.getState(key).equals(value)) {
+				return this.entities.remove(index);
+			}
+			index++;
+		}
+		return null;
+	}
+	
+	/**
+	 * Performs validations on all enitities
+	 * @return true if all entities are valid
+	 */
+	public boolean validate() {
+		boolean valid = true;
+		for (Model entity : this.entities) {
+			valid &= entity.validate();
+		}
+		return valid;
+	}
+
+	/**
+	 * Saves all entities 
+	 * @return true if all saved successfully
+	 */
+	public boolean save() {
+		if(!validate()){
+			return false;
+		}
+		boolean success = true;
+		for (Model entity : this.entities) {
+			success &= entity.save();
+		}
+		return success;
+	}
+	
+	/**
+	 * @return true if all entities are persisted
+	 */
+	public boolean isPersisted() {
+		boolean persisted = false;
+		for (Model entity : this.entities) {
+			persisted |= entity.isPersisted();
+		}
+		return persisted;	
+	}
+
+	/**
+	 * Sorts entities in collection using provided comparator
+	 * @param comparator
+	 */
 	public void sort(Comparator<T> comparator) {
 		Collections.sort(this.entities, comparator);
 	}
 	
+	/**
+	 * Sorts entities in collection in ascending order by value referenced by provided key.
+	 * @param key - column name
+	 */
 	public void sort(final String key) {
 		sort(key, false);
 	}
 	
+	/**
+	 * Sorts entities in collection in ascending order by value referenced by provided key.
+	 * Sorted in descending order if reverse is true.
+	 * @param key - column name
+	 * @param reverse - descending order if true, ascending order if false
+	 */
 	public void sort(final String key, final boolean reverse) {
 		Comparator<T> comparator = new Comparator<T>(){
 
@@ -86,58 +195,46 @@ public abstract class ModelCollection<T extends Model> extends EntityBase {
 		Collections.sort(this.entities, comparator);
 	}
 
-	public void add(T entity) {
-		if(entity != null){
-			this.entities.add(entity);
-		}
+	/**
+	 * @return list of entities in collection
+	 */
+	public List<T> getEntities() {
+		return this.entities;
 	}
 
-	public T get(String key, Object value) {
-		for (T entity : this.entities) {
-			if (entity.getState(key).equals(value)) {
-				return entity;
+	/**
+	 * Returns a property object of schema. Creates it if not exist.
+	 * @return schema
+	 */
+	protected abstract Properties getSchema();
+	
+	/**
+	 * Returns table name.
+	 * @return tableName
+	 */
+	protected abstract String getTableName();
+
+	/**
+	 * Creates entity from state from db. Sets entity as persisted.
+	 * @return entity
+	 */
+	protected abstract T createEntity(Properties persistentState);
+
+	/**
+	 * Fills collections with the results from a query.
+	 * Empties any entities that already exist in collections
+	 * @param result - list of Properties from query
+	 */
+	protected void setEntitiesFromQueryResult(List<?> result){
+		this.entities = new ArrayList<T>();
+		if (result != null) {
+			for(Object state : result){
+				if(state instanceof Properties){
+					T entity = this.createEntity((Properties)state);
+					entity.setPersisted(true);
+					add(entity);
+				}
 			}
 		}
-		return null;
 	}
-	
-	public T remove(String key, Object value) {
-		int index = 0;
-		for (T entity : this.entities) {
-			if (entity.getState(key).equals(value)) {
-				return this.entities.remove(index);
-			}
-			index++;
-		}
-		return null;
-	}
-	
-	public boolean validate() {
-		boolean valid = true;
-		for (Model entity : this.entities) {
-			//TODO add model validation
-			//valid &= entity.validate();
-		}
-		return valid;
-	}
-
-	public boolean save() {
-		if(!validate()){
-			return false;
-		}
-		boolean success = true;
-		for (Model entity : this.entities) {
-			success &= entity.save();
-		}
-		return success;
-	}
-	
-	public boolean isPersisted() {
-		boolean persisted = false;
-		for (Model entity : this.entities) {
-			persisted |= entity.isPersisted();
-		}
-		return persisted;	
-	}	
-
 }
